2d1aea994bea0cfc3571d8fbab9d3a17076e7968
anthony gannuscio <m3t4ldood@gmail.com>
2021-02-22 12:11:03 -0800

    fixed apiRoutes.js

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -3,22 +3,21 @@ const router = require("express").Router();
 const store = require("./../db/store");
 
 // routes
-
-router.get("/notes", function (req, res) {
+router.get("notes", function (req, res) {
     store
         .getNotes()
         .then(notes => res.json(notes))
         .catch(err => res.status(500).json(err))
 });
 
-router.post("/notes", function (req, res) {
+router.post("notes", function (req, res) {
     store
         .addNote(req.body)
         .then((notes) => res.json(notes))
         .catch(err => res.status(500).json(err))
 });
 
-router.delete("/notes/:title", function (req, res) {
+router.delete("notes/:title", function (req, res) {
     store
         .deleteNotes(req.params.title)
         .then(() => res.json({ ok: true }))
