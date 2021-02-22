f37bf85c4a665e2fd16b444e3ba34eb498521e97
anthony gannuscio <m3t4ldood@gmail.com>
2021-02-22 11:57:21 -0800

    added apiRoutes

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,27 @@
+// dependencies
+const router = require("express").Router();
+const store = require("./../db/store");
+
+// routes
+
+router.get("/notes", function (req, res) {
+    store
+        .getNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err))
+});
+
+router.post("/notes", function (req, res) {
+    store
+        .addNote(req.body)
+        .then((notes) => res.json(notes))
+        .catch(err => res.status(500).json(err))
+});
+
+router.delete("/notes/:title", function (req, res) {
+    store
+        .deleteNotes(req.params.title)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err))
+});
+module.exports = router;
